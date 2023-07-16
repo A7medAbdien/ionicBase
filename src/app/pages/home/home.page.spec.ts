b3cd21e1a9d90b0539e1c6d874d2bdbbbef88a77
Ahmed Abdien <ahmed.g.abdien@gmail.com>
2023-07-16 23:14:21 +0300

    T7- TDD navigation, pickup-call and home pages

--- src/app/pages/home/home.page.spec.ts
+++ src/app/pages/home/home.page.spec.ts
@@ -1,15 +1,35 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { HomePage } from './home.page';
+import { Router } from '@angular/router';
 
 describe('HomePage', () => {
   let component: HomePage;
   let fixture: ComponentFixture<HomePage>;
+  let router: Router;
 
   beforeEach(waitForAsync(() => {
     fixture = TestBed.createComponent(HomePage);
+    router = TestBed.inject(Router);
+
     component = fixture.componentInstance;
     fixture.detectChanges();
   }));
+
+  it('should go to pickup call on "see all"', () => {
+    spyOn(router, 'navigate');
+
+    component.goToPickupCall();
+
+    expect(router.navigate).toHaveBeenCalledWith(['pickup-call']);
+  })
+
+  it('should go to pickup calls on "see all"', () => {
+    spyOn(router, 'navigate');
+
+    component.goToPickupCalls();
+
+    expect(router.navigate).toHaveBeenCalledWith(['pickup-calls']);
+  })
 
   it('should create', () => {
     expect(component).toBeTruthy();
--- src/app/pages/pickup-call/pickup-call.page.spec.ts
+++ src/app/pages/pickup-call/pickup-call.page.spec.ts
@@ -1,15 +1,26 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { PickupCallPage } from './pickup-call.page';
+import { Router } from '@angular/router';
 
 describe('PickupCallPage', () => {
   let component: PickupCallPage;
   let fixture: ComponentFixture<PickupCallPage>;
+  let router: Router;
 
   beforeEach(waitForAsync(() => {
     fixture = TestBed.createComponent(PickupCallPage);
+    router = TestBed.inject(Router);
     component = fixture.componentInstance;
     fixture.detectChanges();
   }));
+
+  it('should create a pickup call', () => {
+    spyOn(router, 'navigate');
+
+    component.newPickupCall();
+
+    expect(router.navigate).toHaveBeenCalledWith(['home']);
+  })
 
   it('should create', () => {
     expect(component).toBeTruthy();
