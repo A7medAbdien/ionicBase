81f8dd8297153eddcf2286a67d7034ef9b93ec2b
Ahmed Abdien <ahmed.g.abdien@gmail.com>
2023-07-16 22:17:25 +0300

    T7- TDD navigation
    only loader and login page
    stop using ion-label in login page

--- src/app/pages/loader/loader.page.spec.ts
+++ src/app/pages/loader/loader.page.spec.ts
@@ -1,17 +1,34 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { AppRoutingModule } from './../../app-routing.module';
+import { ComponentFixture, fakeAsync, TestBed, tick, waitForAsync } from '@angular/core/testing';
+import { Router } from '@angular/router';
+import { IonicModule } from '@ionic/angular';
+
 import { LoaderPage } from './loader.page';
 
 describe('LoaderPage', () => {
   let component: LoaderPage;
   let fixture: ComponentFixture<LoaderPage>;
+  let router: Router;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [LoaderPage],
+      imports: [IonicModule.forRoot(),
+        AppRoutingModule]
+    }).compileComponents();
+
     fixture = TestBed.createComponent(LoaderPage);
+    router = TestBed.inject(Router);
     component = fixture.componentInstance;
     fixture.detectChanges();
   }));
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  it('should go to login page', fakeAsync(() => {
+    spyOn(router, 'navigate');
+    component.ngOnInit();
+
+    tick(1500);
+    // expect(component).toBeTruthy();
+    expect(router.navigate).toHaveBeenCalledWith(['login']);
+  }));
 });
--- src/app/pages/loader/loader.page.ts
+++ src/app/pages/loader/loader.page.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-loader',
   templateUrl: './loader.page.html',
   styleUrls: ['./loader.page.scss'],
 })
-export class LoaderPage implements OnInit {
 
-  constructor() { }
+export class LoaderPage implements OnInit {
+  constructor(private router: Router) { }
 
   ngOnInit() {
+    this.navigate();
   }
 
+  navigate() {
+    setTimeout(() => {
+      this.router.navigate(['login']);
+    }, 1000);
+  }
 }
