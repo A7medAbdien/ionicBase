6ad072da70442811351dd8c14ca81bc9a5880d7f
Ahmed Abdien <ahmed.g.abdien@gmail.com>
2023-07-19 04:49:08 +0300

    T10 - create store logic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { LoadingComponent } from './shared/components/loading/loading.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
@@ -6,11 +7,16 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { AppStoreModule } from './shared/store/AppStoreModule';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  declarations: [AppComponent, LoadingComponent],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,
+    ...AppStoreModule,
+    StoreDevtoolsModule.instrument({ maxAge: 25 })
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
